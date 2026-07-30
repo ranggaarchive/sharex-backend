90a47ba252a3ff18dab18a8de7c80dba1de56477
rangga <ranggabp17@gmail.com>
2026-07-30 23:04:00 +0700

    Fix requireAuth import

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const accountService = require('../services/account.service');
-const { requireAuth } = require('../middlewares/auth');
+const { authenticate } = require('../middleware/auth');
 
 /**
  * GET /api/accounts
  * List all active accounts based on user plan
  */
-router.get('/', requireAuth, async (req, res, next) => {
+router.get('/', authenticate, async (req, res, next) => {
   try {
     const userPlan = req.user.plan;
     const accounts = await accountService.listAccounts(userPlan);
